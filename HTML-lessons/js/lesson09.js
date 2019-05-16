2ff847df3dd41cde091265da8d81b45a6b050d37
Gvvs <anjohn@gmx.com>
2019-05-16 20:03:47 +0800

    testing the quokka.js plugin

--- HTML-lessons/js/lesson09.js
+++ HTML-lessons/js/lesson09.js
@@ -1,24 +1,23 @@
 /*jshint esversion: 6 */
 // This is a Javascript object
 var person = {
-  name:'Andrew',
-  age:25,
-  likesPizza:true
+  name: "Andrew",
+  age: 25,
+  likesPizza: true
 };
 
-var putItHere=document.getElementById('thisTag');
-putItHere.innerHTML="person.likesPizza: " + person.likesPizza; // This way of accessing property is called 'dot notation'
-console.log("person['likesPizza']: "+person['likesPizza']); // This way is called 'bracket notation'.
+var putItHere = document.getElementById("thisTag");
+putItHere.innerHTML = "person.likesPizza: " + person.likesPizza; // This way of accessing property is called 'dot notation'
+console.log("person['likesPizza']: " + person["likesPizza"]); // This way is called 'bracket notation'.
 
 // This is a Javascript array
-let colorsILike=['red','blue','purple','green','orange'];
-colorsILike.push('gold');
-colorsILike.unshift('silver');
-colorsILike[3]='pink';
+let colorsILike = ["red", "blue", "purple", "green", "orange"];
+colorsILike.push("gold");
+colorsILike.unshift("silver");
+colorsILike[3] = "pink";
 
 // These are Javascript function
-function greet(name){
-  console.log('Hello '+name);
+function greet(name) {
+  console.log("Hello " + name);
 }
-greet('John');
-
+greet("John");
